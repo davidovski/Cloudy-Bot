aae8925d3ec95cf06d065c9ab3cc9e837fa596fb
david <david@sendula.com>
2017-10-05 17:46:57 +0100

    let there be light!

---
+++ src/main/java/com/mouldycheerio/discord/bot/commands/SetChatChannelCommand.java
@@ -0,0 +1,25 @@
+package com.mouldycheerio.discord.bot.commands;
+
+import com.mouldycheerio.discord.bot.MyBot;
+import com.mouldycheerio.discord.bot.PeelingUtils;
+
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IMessage;
+
+public class SetChatChannelCommand extends BotCommand {
+    public SetChatChannelCommand() {
+        setName("setChatChannel");
+        setDescription(new CommandDescription("setChatChannel", "Set the chatting channel.", "setChatChannel #channel"));
+    }
+
+    public void onCommand(MyBot bot, IDiscordClient client, IMessage commandMessage, String[] args) {
+        if (commandMessage.getAuthor().getStringID().equals(commandMessage.getGuild().getOwner().getStringID())) {
+            IChannel ch = PeelingUtils.channelMentionToId(args[1], commandMessage.getGuild());
+            bot.getChatchannels().put(ch.getStringID());
+            commandMessage.reply("oki, I will reply to all messages there.");
+            bot.saveAll();
+        }
+
+    }
+}
